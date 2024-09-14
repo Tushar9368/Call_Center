6b068eac85d30b7fda194e6ba47d052d5179f763
Tushar9368 <148549270+Tushar9368@users.noreply.github.com>
2024-09-14 19:21:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const queryParams = new URLSearchParams({
       'api-key': apiKey,
       'format': 'json',
-      'limit': 10000,
+      'limit': 5000,
       ...filters
     });
 
